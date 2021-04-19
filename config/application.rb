da4d01542d071cf74613ae731c1b19ca7a314b2e
Sara Lopez <slopgon@gmail.com>
2021-04-19 17:05:02 +0200

    change form error message from a div.field_with_errors to an inline error class

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,14 @@ module Shiftwork2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+      class_attr_index = html_tag.index 'class="'
+      if class_attr_index
+        html_tag.insert class_attr_index+7, 'error '
+      else
+        html_tag.insert html_tag.index('>'), ' class="error"'
+      end
+    end
   end
 end
