8e8773bd6005ddc2b3d966861bff1ca4bc654cc9
Sara Lopez <slopgon@gmail.com>
2021-04-29 18:00:39 +0200

    change flash alert on verify access to warning to have a yellow bkg message

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -50,7 +50,7 @@ class OrganizationsController < ApplicationController
   # only the organization logged in has access to the organization actions
   def verify_access
     unless current_user.id == @organization.user_id
-      flash[:alert] = "You do not have authority to access that."
+      flash[:warning] = "You do not have authority to access that."
       redirect_to user_path(current_user.id)
     end
   end
