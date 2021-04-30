913a9bfc9bf429b9bd55b076443578054990782a
Arit Amana <aritamana@zoho.com>
2021-04-29 20:03:16 -0400

    Fix bootstrap styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+import 'bootstrap'
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 require("../../assets/stylesheets/application.scss")
