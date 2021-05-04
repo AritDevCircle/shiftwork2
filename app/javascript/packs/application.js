778faa074b01f2111951b41a730413e2a84a9857
timibello <timilehindebra@gmail.com>
2021-05-04 14:43:52 -0500

    resolved conflicts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,8 @@ require('jquery')
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 require("../../assets/stylesheets/application.scss")
-//= require jquery
-//= require jquery_ujs
+    //= require jquery
+    //= require jquery_ujs
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
