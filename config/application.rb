6c71a6de70c372b0c513d66132547447bf889f58
timibello <timilehindebra@gmail.com>
2021-05-05 16:38:57 -0500

    resolved conflicts

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,11 @@ module Shiftwork2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.exceptions_app = self.routes
-    # Settings in config/environments/* take precedence over those specified here.
+    
+    # Set official app timezone
+    config.time_zone = "UTC"
+
+# Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
