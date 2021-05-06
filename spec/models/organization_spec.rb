d5a21d4cbb2fd16c0dabc8517e7ade2dd8f2b261
Gabrielle <eastongabrielle@gmail.com>
2021-05-06 14:35:21 -0400

    added tests for org model

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Organization, type: :model do
     it { is_expected.to validate_presence_of(:org_address) }
     it { is_expected.to validate_presence_of(:org_city) }
     it { is_expected.to validate_presence_of(:org_state) }
-    # it { is_expected.to validate_uniqueness_of(:org_name) }
+    it { should validate_uniqueness_of(:org_name).case_insensitive }
     it { is_expected.to validate_length_of(:org_description).is_at_least(10) }
   end
 
