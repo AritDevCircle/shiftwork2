aa2aacf5a3c9f7b01845fd37af14751399a2a44d
Gabrielle <eastongabrielle@gmail.com>
2021-05-07 13:56:11 -0400

    updated syntax for 2 tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe Organization, type: :model do
     it { is_expected.to validate_presence_of(:org_address) }
     it { is_expected.to validate_presence_of(:org_city) }
     it { is_expected.to validate_presence_of(:org_state) }
-    it { should validate_uniqueness_of(:org_name).case_insensitive }
-    it { should validate_uniqueness_of(:org_address).case_insensitive }
+    it { is_expected.to validate_uniqueness_of(:org_name).case_insensitive }
+    it { is_expected.to validate_uniqueness_of(:org_address).case_insensitive }
     it { is_expected.to validate_length_of(:org_description).is_at_least(10) }
   end
   
