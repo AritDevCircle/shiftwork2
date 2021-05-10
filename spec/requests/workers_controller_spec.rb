4bfc9c5d3a5ab5622c074ad9b6fa2db14aaa0ccc
ncbui <ncbui@users.noreply.github.com>
2021-05-10 12:05:11 -0700

    Lay out worker routes

--- spec/requests/workers_controller_spec.rb
+++ spec/requests/workers_controller_spec.rb
@@ -1,7 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "WorkersControllers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /workers" do
+    # workers #index    
   end
+
+  describe "POST /workers" do
+    # workers #create
+  end
+
+  describe "GET /workers/new" do
+    #  workers #new
+  end
+
+  describe "GET /workers/:id" do
+    #   workers #show
+end
+
+describe "GET /workers/:id/edit" do
+    #   workers #edit
+  end
+
+  describe "PATCH /workers/:id" do
+    # workers # partial update
+  end
+
+  describe "PUT /workers/:id" do
+    # workers # full update
+  end
+
+  describe "DELETE /workers/:id" do
+    #   workers #destroy
+  end
+
+
 end
