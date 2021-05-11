98fdbe8c37226140fa8ab288febad634829ee6d9
Gabrielle <eastongabrielle@gmail.com>
2021-05-11 16:26:53 -0400

    changed filled_by(user) method

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Shift, type: :model do
 
 
   describe "shift_org_name method" do
-    it "should return name of org that owns the shift" do
+    it "should return the name of the organization that owns shift" do
       sample_shift = create(:shift, :front_of_house_role, :open_shift)
       shift_org = Organization.find(sample_shift.organization_id)
       expect(sample_shift.shift_org_name).to eq(shift_org.org_name)
@@ -26,7 +26,7 @@ RSpec.describe Shift, type: :model do
   end
 
   describe "shift_worker_name method" do
-    it "should return name of worker assigned to shift" do 
+    it "should return the name of the worker assigned to shift" do 
       sample_user = create(:user, :worker_user_type)
       sample_worker = create(:worker, user_id: sample_user.id)
       sample_shift = create(:shift, :bartender_role, :filled_shift, worker_id: sample_worker.id)
@@ -35,7 +35,7 @@ RSpec.describe Shift, type: :model do
   end
 
   describe "filled_by(user) method" do
-    it "should check if worker assigned to shift is same as current user" do
+    it "should check if the worker assigned to the shift is the same as the current user" do
     sample_user = create(:user, :worker_user_type)
     sample_worker = create(:worker, user_id: sample_user.id)
     sample_shift = create(:shift, :bartender_role, :filled_shift, worker_id: sample_worker.id)
@@ -44,12 +44,12 @@ RSpec.describe Shift, type: :model do
   end
 
   describe "can_be_dropped? method" do 
-    it "should return true when shift starts 24 hours or more from current time" do
+    it "should return true when the shift starts 24 hours or more from the current time" do
       sample_shift = create(:shift, :chef_role, :filled_shift, shift_start: DateTime.now + 72.hours, shift_end: DateTime.now + 78.hours)
       expect(sample_shift.can_be_dropped?).to be(true)
     end
 
-    it "should return false when shift starts less than 24hrs from current time" do
+    it "should return false when the shift starts less than 24hrs from the current time" do
       sample_shift = create(:shift, :chef_role, :filled_shift, shift_start: DateTime.now + 10.hours, shift_end: DateTime.now + 14.hours)
       expect(sample_shift.can_be_dropped?).to be(false)
     end
