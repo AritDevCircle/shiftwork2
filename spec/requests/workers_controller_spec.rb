b032fba4b9d62be1fa6b04a300148d14a8000607
ncbui <ncbui@users.noreply.github.com>
2021-05-12 03:32:10 -0700

    progress with worker#create

--- spec/requests/workers_controller_spec.rb
+++ spec/requests/workers_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "WorkersControllers", type: :request do
-  let(:user) { create(:user) }
-  let(:worker) { create(:worker) }
+  let(:org_user) { create(:user) }
+  let(:worker_user) { create(:user, :worker_user_type) }
+  let(:worker) { create(:worker, user_id: worker_user.id) }
+  let(:org) { create(:org, user_id: org_user.id) }
 
   describe "GET /workers" do
     # workers #index    
@@ -14,12 +16,14 @@ RSpec.describe "WorkersControllers", type: :request do
     # workers #create
     before do
       Worker.destroy_all
+      login_as(worker_user.email, worker_user.password)
     end
-    # FIXME: getting a failure, redirect goes to login instead, why?
-    it "should create a new worker when all necessary params are supplied" do
-      post workers_path, params: { worker: { user_id: user.id, first_name: "Full", last_name: "Name", worker_city: "Cityville", worker_state:"AA", bio: ""} }
 
-      expect(response).to redirect_to users_path
+    it "should create a new worker when all necessary params are supplied" do
+      post workers_path, params: { worker: { user_id: worker_user.id, first_name: "Full", last_name: "Name", worker_city: "Cityville", worker_state:"AA", bio: ""} }
+      
+      # FIXME: getting a failure, redirect goes to login instead, why?
+      # expect(response).to redirect_to users_path(worker_user.id)
       expect(response).to have_http_status(302)
 
       follow_redirect!
@@ -28,14 +32,11 @@ RSpec.describe "WorkersControllers", type: :request do
       expect(response.body).to include("Your Worker Account Details")
     end
 
-    fit "should display an erorr message and re-render the form if necessary params are missing" do
-      post workers_path, params: { worker: { user_id: user.id }}
-      expect(response).to redirect_to users_path
-      expect(response).to have_http_status(302)
-
-      follow_redirect!
-
+    it "should display an error message and re-render the form if necessary params are missing" do
+      post workers_path, params: { worker: { first_name: nil}}
+      
       expect(response.body).to include("Something went wrong.")
+      expect(response.body).to include("Create Worker Account")
     end
     # users must be logged in to access
     # with errors
@@ -55,13 +56,30 @@ RSpec.describe "WorkersControllers", type: :request do
 
   describe "GET /workers/:id" do
     #   workers #show
+
+    it "should show a worker their shifts" do
+      login_as(worker_user.email, worker_user.password)
+      get workers_path(worker.id)
+
+      # expect(response).to have_http_status(204)
+      expect(response.body).to include("All Your Shifts")
+
+    end
+
+    it "should show users who are not the worker a bio" do
+      login_as(org_user.email, org_user.password)
+      get workers_path(worker.id)
+
+      expect(response.body).to include(worker.bio)
+    end
+
     # as an org or other worker
         # Shows name and bio
     # as the worker
         # "All Your Shifts"
-end
+  end
 
-describe "GET /workers/:id/edit" do
+  describe "GET /workers/:id/edit" do
     #   workers #edit
     # as an org
         # "You do not have authority to access that."
