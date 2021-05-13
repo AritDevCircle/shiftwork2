d064d2fb877801601f3f98f4cc60d5ddf3ad2250
ncbui <ncbui@users.noreply.github.com>
2021-05-12 23:45:57 -0700

    Update tests

--- spec/requests/workers_controller_spec.rb
+++ spec/requests/workers_controller_spec.rb
@@ -1,42 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe "WorkersControllers", type: :request do
-  let(:org_user) { create(:user) }
-  let(:worker_user) { create(:user, :worker_user_type) }
-  let(:worker) { create(:worker, user_id: worker_user.id) }
-  let(:org) { create(:org, user_id: org_user.id) }
+  let(:sample_org_user) { create(:user) }
+  let(:sample_worker_user) { create(:user, :worker_user_type) }
+  let(:sample_worker) { create(:worker, user_id: sample_worker_user.id) }
+  let(:sample_org) { create(:organization, user_id: sample_org_user.id) }
 
-  describe "GET /workers/new" do
-    it "should display Create Worker Account form when user is a worker without a worker account" do
+  describe "GET /workers/new action" do
+
+    it "should successfully display Create Worker Account form when user has a worker type and does not have a worker account" do
       Worker.destroy_all
-      login_as(worker_user.email, worker_user.password)
+      login_as(sample_worker_user.email, sample_worker_user.password)
       get new_worker_path
       
       expect(response).to have_http_status(200)
+
       expect(response.body).to include("Create Worker Account")
     end
     
-    # as an org, TODO: orgs can access /workers/new page, check with group
-    it "should display error message and redirect to root_path if user is a worker with an existing worker account" do
-      
+    it "should display error message and redirect to user page if user is a worker with an existing worker account" do
+      login_as(sample_worker_user.email, sample_worker_user.password)
+      get new_worker_path
+
+      expect(response).to have_http_status(302)
+
+      follow_redirect!
+
+      expect(response.body).to include("You are unauthorized to create new worker.")
+      expect(response.body).to include("Create Worker Account")
     end
 
-    it "should display error message and redirect to root_path if user is an org" do
+    it "should display error message and redirect to user page if user is an org" do
+      login_as(sample_org_user.email, sample_org_user.password)
+      get new_worker_path
 
+      expect(response).to have_http_status(302)
+
+      follow_redirect!
+
+      expect(response.body).to include("You are unauthorized to create new worker.")
+      expect(response.body).to include("Create Worker Account")
     end
   end
 
-  describe "POST /workers" do
+  describe "POST /workers action" do
     before do
       Worker.destroy_all
-      login_as(worker_user.email, worker_user.password)
+      login_as(sample_worker_user.email, sample_worker_user.password)
     end
 
     it "should create a new worker when all necessary params are supplied" do
-      post workers_path, params: { worker: { user_id: worker_user.id, first_name: "Full", last_name: "Name", worker_city: "Cityville", worker_state:"AA", bio: ""} }
+      post workers_path, params: { worker: { user_id: sample_worker_user.id, first_name: "Full", last_name: "Name", worker_city: "Sample City", worker_state:"AA", bio: "Sample Bio"} }
       
-      expect(response).to redirect_to user_path(worker_user.id)
       expect(response).to have_http_status(302)
+      expect(response).to redirect_to user_path(sample_worker_user.id)
 
       follow_redirect!
 
@@ -44,30 +61,38 @@ RSpec.describe "WorkersControllers", type: :request do
       expect(response.body).to include("Your Worker Account Details")
     end
 
-    it "should display an error message and re-render the form if necessary params are missing" do
-      post workers_path, params: { worker: { first_name: nil}}
+    it "should display the 'Create Shift' form if one or more necessary params are missing" do
+      post workers_path, params: { worker: { first_name: nil } }
       
       expect(response.body).to include("Something went wrong.")
       expect(response.body).to include("Create Worker Account")
     end
   end
 
-  describe "GET /workers/:id" do
-    it "should display shifts that the worker has taken if the user is the worker" do
-      login_as(worker_user.email, worker_user.password)
-      get worker_path(worker.id)
+  describe "GET /workers/:id action" do
+    it "should display shifts that the worker has taken if worker views their page" do
+      login_as(sample_worker_user.email, sample_worker_user.password)
+      get worker_path(sample_worker.id)
 
       expect(response).to have_http_status(200)
       expect(response.body).to include("All Your Shifts")
     end
   end
 
-  describe "GET /workers/:id/edit" do
-    it "should redirect to user's page with an error message if the user is not the worker" do
-      login_as(org_user.email, org_user.password)
-      get edit_worker_path(worker.id)
+  describe "GET /workers/:id/edit action" do
+    it "should successfully display an 'Edit Worker' form to the worker" do
+      login_as(sample_worker_user.email, sample_worker_user.password)
+      get edit_worker_path(sample_worker.id)
 
-      expect(response).to redirect_to user_path(org_user.id)
+      expect(response).to have_http_status(200)
+      expect(response.body).to include("Edit Workers Account")
+    end
+
+    it "should display error and redirect to users page if the user is not the worker" do
+      login_as(sample_org_user.email, sample_org_user.password)
+      get edit_worker_path(sample_worker.id)
+
+      expect(response).to redirect_to user_path(sample_org_user.id)
       expect(response).to have_http_status(302)
 
       follow_redirect!
@@ -76,24 +101,18 @@ RSpec.describe "WorkersControllers", type: :request do
       expect(response.body).to include("Your Email:")
     end
     
-    it "should show a worker a form to edit their account" do
-      login_as(worker_user.email, worker_user.password)
-      get edit_worker_path(worker.id)
-      
-      expect(response).to have_http_status(200)
-      expect(response.body).to include("Edit Workers Account")
-    end
+
 
     # should I test trying to edit a worker as another worker? 
   end
 
   describe "PATCH /workers/:id" do
     it "should update the worker's information" do
-      login_as(worker_user.email, worker_user.password)
+      login_as(sample_worker_user.email, sample_worker_user.password)
 
-      patch worker_path(worker.id), params: { worker: { first_name:"Aegon" } }
+      patch worker_path(sample_worker.id), params: { worker: { first_name:"Aegon" } }
 
-      expect(response).to redirect_to user_path(worker_user.id)
+      expect(response).to redirect_to user_path(sample_worker_user.id)
       expect(response).to have_http_status(302)
 
       follow_redirect!
@@ -104,12 +123,12 @@ RSpec.describe "WorkersControllers", type: :request do
   end
 
   describe "PUT /workers/:id" do
-    it "should update the worker's information" do
-      login_as(worker_user.email, worker_user.password)
+    it "should update the sample_worker's information" do
+      login_as(sample_worker_user.email, sample_worker_user.password)
 
-      put worker_path(worker.id), params: { worker: { user_id: worker_user.id, first_name: "Updated", last_name: "Name", worker_city: "Cityville", worker_state:"AA", bio: ""} }
+      put worker_path(sample_worker.id), params: { worker: { user_id: sample_worker_user.id, first_name: "Updated", last_name: "Name", worker_city: "Cityville", worker_state:"AA", bio: ""} }
 
-      expect(response).to redirect_to user_path(worker_user.id)
+      expect(response).to redirect_to user_path(sample_worker_user.id)
       expect(response).to have_http_status(302)
 
       follow_redirect!
@@ -118,10 +137,4 @@ RSpec.describe "WorkersControllers", type: :request do
       expect(response.body).to include("Updated")
     end
   end
-
-  describe "DELETE /workers/:id" do
-    #   workers #destroy
-    # is this implemented?
-  end
-
 end
