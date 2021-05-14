2478c360f251d1775fdb1402547815f00ec29053
Sara Lopez <slopgon@gmail.com>
2021-05-14 16:19:53 +0200

    improving tests after review

--- spec/requests/organizations_controller_spec.rb
+++ spec/requests/organizations_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "OrganizationsControllers", type: :request do
+RSpec.describe "OrganizationsController", type: :request do
   let(:org_user) { create(:user) }
   let(:worker_user) { create(:user, :worker_user_type) }
 
+  before do
+    Organization.destroy_all
+    Worker.destroy_all
+  end
+
   describe "GET /organizations/new" do
-    it "should display Create Organization form when user is logged in as organization user_type but hasn't the org account yet" do
-      Organization.destroy_all
+    it "should display new form when organization hasn't an account yet" do
       login_as(org_user.email, org_user.password)
       get new_organization_path
 
@@ -14,13 +18,10 @@ RSpec.describe "OrganizationsControllers", type: :request do
       expect(response.body).to include("<h1>Create Organization</h1>")
     end
 
-    # Not merged into main yet so it will fail:
-    it "should display 'You do not have authority to access that.' warning when user is logged in as worker and tries to access the organizations/new page" do
-      Worker.destroy_all
+    it "should display warning when a worker tries to access the new page" do
       login_as(worker_user.email, worker_user.password)
       get new_organization_path
 
-      expect(response).to redirect_to user_path(worker_user.id)
       expect(response).to have_http_status(302)
 
       follow_redirect!
@@ -28,14 +29,11 @@ RSpec.describe "OrganizationsControllers", type: :request do
       expect(response.body).to include("You do not have authority to access that.")
     end
 
-    # Not merged into main yet so it will fail:
-    it "should display 'You have already created your organization.' warning when an organization user has already filled their data" do
-      Organization.destroy_all
+    it "should display warning when an org is already created" do
       login_as(org_user.email, org_user.password)
       test_org = create(:organization, user_id: org_user.id)
       get new_organization_path
 
-      expect(response).to redirect_to user_path(org_user.id)
       expect(response).to have_http_status(302)
 
       follow_redirect!
@@ -44,9 +42,9 @@ RSpec.describe "OrganizationsControllers", type: :request do
     end
   end
 
+  # Running this to test the private verify_access method inside the org. controller 
   describe "GET /organizations/:id/edit" do
-    it "should display Edit Organization form when user is logged in as an organization" do
-      Organization.destroy_all
+    it "should display Edit Org form when user is an organization" do
       login_as(org_user.email, org_user.password)
       test_org = create(:organization, user_id: org_user.id)
       get edit_organization_path(test_org.id)
@@ -55,13 +53,11 @@ RSpec.describe "OrganizationsControllers", type: :request do
       expect(response.body).to include('value="123 Some Street"')
     end
 
-    it "should redirect to user page and display \"You do not have authority to access that.\" alert when user is logged in as a worker" do
-      Organization.destroy_all
+    it "should redirect and display alert when user is a worker" do
       login_as(worker_user.email, worker_user.password)
       test_org = create(:organization, user_id: org_user.id)
       get edit_organization_path(test_org.id)
 
-      expect(response).to redirect_to user_path(worker_user.id)
       expect(response).to have_http_status(302)
   
       follow_redirect!
@@ -72,8 +68,7 @@ RSpec.describe "OrganizationsControllers", type: :request do
 
   # show
   describe "GET /organizations/:id" do
-    it "should display Organization info when user is logged in as the current organization" do
-      Organization.destroy_all
+    it "should display Org info when user is the current organization" do
       login_as(org_user.email, org_user.password)
       test_org = create(:organization, user_id: org_user.id)
       get organization_path(test_org.id)
@@ -84,8 +79,7 @@ RSpec.describe "OrganizationsControllers", type: :request do
       expect(response.body).to include("Create Shift")
     end
 
-    it "should display basic Organization info when user is logged in as a worker" do
-      Organization.destroy_all
+    it "should display basic Org info when user is a worker" do
       login_as(worker_user.email, worker_user.password)
       test_org = create(:organization, user_id: org_user.id)
       get organization_path(test_org.id)
@@ -100,14 +94,12 @@ RSpec.describe "OrganizationsControllers", type: :request do
   # create
   describe "POST /organizations" do
     before do
-      Organization.destroy_all
       login_as(org_user.email, org_user.password)
     end
 
-    it "should create a new organization when all necessary params are supplied" do
+    it "should create a new org when all necessary params are supplied" do
       post organizations_path, params: { organization: { org_name: "Test org name", org_description: "Test org description", org_address: "Test org address", org_city: "The City", org_state: "TO" } }
 
-      expect(response).to redirect_to user_path(org_user.id)
       expect(response).to have_http_status(302)
 
       follow_redirect!
@@ -115,7 +107,7 @@ RSpec.describe "OrganizationsControllers", type: :request do
       expect(response.body).to include("Organization created successfully!")
     end
 
-    it "should display the 'Create organization' view if one or more necessary params are missing" do
+    it "should display the 'Create org' view if one or more necessary params are missing" do
       post organizations_path, params: { organization: { org_name: "Test org name", org_description: "Test org description", org_address: "", org_city: "The City", org_state: "TO" } }
 
       expect(response.body).to include("Something went wrong.")
@@ -126,7 +118,6 @@ RSpec.describe "OrganizationsControllers", type: :request do
   # update
   describe "PATCH /organizations/:id" do
     before do
-      Organization.destroy_all
       login_as(org_user.email, org_user.password)
     end
 
@@ -134,7 +125,6 @@ RSpec.describe "OrganizationsControllers", type: :request do
       test_org = create(:organization, user_id: org_user.id)
       patch organization_path(test_org.id), params: { organization: { org_city:"Some other City" } }
 
-      expect(response).to redirect_to user_path(org_user.id)
       expect(response).to have_http_status(302)
 
       follow_redirect!
