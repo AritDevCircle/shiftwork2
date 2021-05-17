f25a170939f7fa0a4c49bc6736f6fa5e4361d961
Arit Amana <aritamana@zoho.com>
2021-05-17 19:28:23 -0400

    complete specs

--- spec/requests/shifts_controller_spec.rb
+++ spec/requests/shifts_controller_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "ShiftsControllers", type: :request do
   let(:sample_org) { create(:organization) }
-  let(:sample_user) { create(:user) }
-  let(:sample_worker) { create(:worker, user_id: sample_worker.id) }
+  let(:sample_org_user) { create(:user) }
+  let(:sample_worker_1) { create(:worker) }
+  let(:sample_worker_2) { create(:worker) }
+  let(:sample_shift_1) { create(:shift, :chef_role, :filled_shift, worker_id: sample_worker_1.id) }
+  let(:shift_starting_soon) { create(:shift, :chef_role, :filled_shift, worker_id: sample_worker_2.id, shift_start: DateTime.now + 6.hours, shift_end: DateTime.now + 18.hours) }
 
   describe "GET /index action" do
     it "should redirect to organization show page if current_user.has_org?" do
@@ -18,9 +21,9 @@ RSpec.describe "ShiftsControllers", type: :request do
       expect(response.body).to include("Your Shifts")
     end
 
-    it "should show all shifts if current_user does not have org" do
+    it "should show all shifts if current_user does not have_org?" do
       shift1 = create(:shift, :chef_role, :open_shift, organization_id: sample_org.id)
-      login_as(sample_user.email, "password123")
+      login_as(sample_org_user.email, "password123")
       get shifts_path
 
       expect(response.body).to include("List of All Shifts")
@@ -29,7 +32,7 @@ RSpec.describe "ShiftsControllers", type: :request do
   end
   
   describe "GET /shifts/new action" do
-    it "should successfully display Create Shift form if current_user has org" do
+    it "should successfully display Create Shift form if current_user has_org?" do
       org_user = User.find_by(id: sample_org.user_id)
       login_as(org_user.email, "password123")
       get new_shift_path
@@ -38,8 +41,8 @@ RSpec.describe "ShiftsControllers", type: :request do
       expect(response.body).to include("Create Shift")
     end
 
-    it "should display error and redirect to shifts page if current_user does not have org" do
-      login_as(sample_user.email, "password123")
+    it "should display error and redirect to shifts page if current_user does not have_org?" do
+      login_as(sample_org_user.email, "password123")
       get new_shift_path
 
       expect(response).to have_http_status(302)
@@ -82,10 +85,17 @@ RSpec.describe "ShiftsControllers", type: :request do
 
   describe "PATCH /shifts/update action" do
     it "should update the shift's information" do
+      org_user = User.find_by(id: sample_org.user_id)
+      login_as(org_user.email, "password123")
+
       sample_shift = create(:shift, :chef_role, organization_id: sample_org.id, shift_open: true)
-      login_as(sample_user.email, "password123")
+
+      get organization_path(sample_org.id)
+
+      expect(response.body).to include("Chef")
+      expect(response.body).to include("Cook all the food")
       
-      patch shift_path(sample_org.id), params: { shift: { shift_role: "Bartender", shift_description: "Mix all the drinks", shift_start: "2021-12-30 12:00:00", shift_end: "2021-12-30 15:00:00", shift_pay: "22", shift_open: true } }
+      patch shift_path(sample_shift.id), params: { shift: { shift_role: "Bartender", shift_description: "Mix all the drinks", shift_start: "2021-12-30 12:00:00", shift_end: "2021-12-30 15:00:00", shift_pay: "22", shift_open: true } }
       
       expect(response).to have_http_status(302)
 
@@ -95,14 +105,19 @@ RSpec.describe "ShiftsControllers", type: :request do
     end
 
     it "should display the 'Update Shift' form if one or more necessary params are missing" do
+      org_user = User.find_by(id: sample_org.user_id)
+      login_as(org_user.email, "password123")
+      
       test_shift = create(:shift, :chef_role, organization_id: sample_org.id, shift_open: true)
-      login_as(sample_user.email, "password123")
 
-      patch shift_path(sample_shift.id), params: { shift: { shift_role: "Bartender", shift_description: "Mix all the drinks", shift_start: "2021-12-30 12:00:00", shift_end: "2021-12-30 15:00:00", shift_pay: "22", shift_open: true } }
+      get organization_path(sample_org.id)
 
-      expect(response).to have_http_status(302)
+      expect(response.body).to include("Chef")
+      expect(response.body).to include("Cook all the food")
 
-      follow_redirect!
+      patch shift_path(test_shift.id), params: { shift: { shift_role: "Bartender", shift_description: "", shift_start: "2021-12-30 12:00:00", shift_end: "2021-12-30 15:00:00", shift_pay: "22", shift_open: true } }
+
+      expect(response).to have_http_status(422)
 
       expect(response.body).to include("Shift description is too short (minimum is 10 characters)")
       expect(response.body).to include("Update Shift")
@@ -110,40 +125,31 @@ RSpec.describe "ShiftsControllers", type: :request do
   end
 
   describe "DELETE /shifts action" do
-    it "should delete a shift" do
-      test_shift = create(:shift, :chef_role, organization_id: sample_org.id, shift_open: true)   
-      login_as(sample_user.email, "password123")
+    it "should delete the shift, display success message and redirect to shifts list" do
+      org_user = User.find_by(id: sample_org.user_id)
+      login_as(org_user.email, "password123")
+      test_shift_1 = create(:shift, :chef_role, organization_id: sample_org.id, shift_open: true)
+      test_shift_2 = create(:shift, :bartender_role, organization_id: sample_org.id, shift_open: true)
 
-      delete shift_path(sample_shift.id)
+      delete shift_path(test_shift_1.id)
   
       expect(response).to have_http_status(302)
-      expect(response.body).to include("Delete")
 
       follow_redirect!
   
+      expect(response.body).to include("Delete")
       expect(response.body).to include("Shift deleted successfully!")
-    end
-  
-    it "should display alert message and redirect to shifts list" do
-      test_shift = create(:shift, :chef_role, organization_id: sample_org.id, shift_open: true)    
-      login_as(sample_user.email, "password123")
-
-      delete shift_path(sample_shift.id)
-  
-      expect(response).to have_http_status(302)
-
-      follow_redirect!
-      
-      expect(response.body).to include("Delete")
+      expect(response.body).to include("Mix all the drinks")
+      expect(response.body).not_to include("Cook all the food")
     end
   end 
 
   describe "PATCH /shifts/take action" do
     it "should display error message if shift is already taken by a worker" do
-      sample_worker = create(:worker, :first_name, worker_id: sample_worker.id, shift_open: false) 
-      login_as(sample_worker_user.email, "password123")
-
-      patch take_shift_path(sample_worker.id)
+      worker_user = User.find_by(id: sample_worker_2.user_id)
+      login_as(worker_user.email, "password123")
+      
+      patch take_shift_path(sample_shift_1.id)
 
       expect(response).to have_http_status(302)
 
@@ -153,51 +159,65 @@ RSpec.describe "ShiftsControllers", type: :request do
     end
 
     it "should redirect to worker_path and display success message if current_worker takes a shift" do
-     sample_worker = create(:worker, :first_name, worker_id: sample_worker.id, shift_open: false) 
-      login_as(sample_worker_user.email, "password123")
+      sample_shift_2 = create(:shift, :bartender_role, :open_shift)
+      sample_worker_3 = create(:worker)
+      worker_user = User.find_by(id: sample_worker_3.user_id)
+      login_as(worker_user.email, "password123")
 
-      patch take_shift_path(sample_worker.id)
+      patch take_shift_path(sample_shift_2.id)
 
       expect(response).to have_http_status(302)
 
       follow_redirect!
 
       expect(response.body).to include("Shift has been assigned to you successfully!")
+
+      sample_shift_2.reload
+
+      expect(sample_shift_2.shift_open).to be(false)
+      expect(sample_shift_2.worker_id).to eq(sample_worker_3.id)
     end
   end
 
   describe "PATCH /shifts/drop action" do
     it "should display error message and not allow worker drop shift if current_worker isn't assigned to shift" do
-      sample_worker = create(:worker, :first_name, worker_id: sample_worker.id, shift_open: false) 
-      login_as(sample_worker_user.email, "password123")
+      sample_worker_3 = create(:worker)
+      worker_user = User.find_by(id: sample_worker_3.user_id)
+      login_as(worker_user.email, "password123")
 
-      patch drop_shift_path(sample_worker.id)
+      patch drop_shift_path(sample_shift_1.id)
 
       expect(response).to have_http_status(302)
+
+      follow_redirect!
+      
       expect(response.body).to include("Shift is NOT assigned to you; cannot drop.")
     end
 
-    it "should display error message and not allow worker drop shift if Shift starts in less than 24 hours" do
-      sample_worker = create(:worker, :first_name, worker_id: sample_worker.id, shift_open: false) 
-      login_as(sample_worker_user.email, "password123")
+    it "should display error message and not allow worker drop shift if it starts within 24 hours" do
+      worker_user = User.find_by(id: sample_worker_2.user_id)
+      login_as(worker_user.email, "password123")
 
-      patch drop_shift_path(sample_worker.id)
-
-      expect(response).to have_http_status(302)
-      expect(response.body).to include("Shift starts in less than 24 hours; cannot drop.")
-    end
-
-    it "should redirect to worker_path and display success message if shift is open and current_worker drops a shift" do
-     sample_worker = create(:worker, :first_name, worker_id: nil, shift_open: true) 
-      login_as(sample_worker_user.email, "password123")
-
-      patch worker_path(current_user.worker_account.id)
+      patch drop_shift_path(shift_starting_soon.id)
 
       expect(response).to have_http_status(302)
 
       follow_redirect!
 
-      expect(response.body).to include("Shift has been assigned to you successfully!")
+      expect(response.body).to include("Shift starts in less than 24 hours; cannot drop.")
+    end
+
+    it "should redirect to worker_path and display success message if shift dropped successfully" do
+      worker_user = User.find_by(id: sample_worker_1.user_id)
+      login_as(worker_user.email, "password123")
+
+      patch drop_shift_path(sample_shift_1.id)
+
+      expect(response).to have_http_status(302)
+
+      follow_redirect!
+
+      expect(response.body).to include("Shift has been dropped successfully!")
     end
   end
 end
